804b8988368aeba9f8a8be8c12ae8e7515cd9ffe
Fernando Azevedo <azevedo-fernando@users.noreply.github.com>
2020-07-04 12:40:50 -0300

    DESMEMBRAMENTO 04/07/20

---
+++ LaunchBase/MODULO03/data.js
@@ -0,0 +1,40 @@
+//Transformando em dados os arquivos do portfolio
+// exportando os dados 
+module.exports = [ 
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado JS puro e CSS Animation | Mayk Brito",
+        duration:"57 min",
+        price:"Free"
+    },  
+    {
+        id:"2alg7MQ6_sI",
+        title:"Como usar Git e Github na prática: Guia para iniciantes | Mayk Brito",
+        duration:"33 min",
+        price:"Free"
+    },
+    {
+        id:"HN1UjzRSdBk",
+        title:"Desvendando o CSS Grid na prática | Mayk Brito",
+        duration:"36 min",
+        price:"Free"
+    },
+    {
+        id:"ghTrp1x_1As",
+        title:"O que é API? REST e RESTful? | Mayk Brito",
+        duration:"57 min",
+        price:"Free"
+    },
+    {
+        id:"c7P03kkrEG8",
+        title:"Configurando VSCode para Node.js, ReactJS e React Native | Diego Fernandes",
+        duration:"57 min",
+        price:"Free"
+    },
+    {
+        id:"rAzHvYnQ8DY",
+        title:"Array: Higher Order Functions | Mayk Brito",
+        duration:"57 min",
+        price:"Free"
+    }
+]
--- LaunchBase/MODULO03/server.js
+++ LaunchBase/MODULO03/server.js
@@ -1,10 +1,25 @@
-const express = require('express') //importando a dependencia do express
+const express = require("express") //importando a dependencia do express
+const nunjucks = require("nunjucks") // Template engine // reuso de códigos // 
+
 const server = express() // criando um servidor, executando o express como função
+const videos = require("./data") // o ./ se refere a raiz do projeto, sem necessidade de colocar a extensão do arquivo
+//ARRAY CRIADO EM data.js 
+server.use(express.static('public')) // express observando a pasta "public", para servir o arquivos estáticos
+
+server.set("view engine", "njk") //Configurando a Template Engine //setando o motor de view para NJK (antes como HTML)
+
+nunjucks.configure("views", { //Configurando o caminho "views" , opções em objeto, usando o express com a variável que ele está utilizando
+    express: server
+})
 
 server.get("/", function(req, res) { //adicionando a rota "/" função REQ: requisição RES: resposta do servidor
-    return res.send("Hi!") // respondendo
+    return res.render("about") // respondendo com a renderização do about
+})
+
+server.get("/portfolio", function(req, res) { //adicionando a rota "/" função REQ: requisição RES: resposta do servidor
+    return res.render("portfolio", {items: videos}) // respondendo com a renderização do portfolio + os vídeos
 })
 
 server.listen(5000, function() { //servidor fica ouvindo a porta 5000 // CALLBACK = função dentro de uma outra função
-    console.log('server is running')
+    console.log("server is running")
 })
